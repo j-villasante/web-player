8615a589e44db070874d8265866b60f1a6ca3ae2
Josue Villasante <javillasantet@gmail.com>
2017-03-14 01:39:02 -0500

    wip on importer.js

--- controllers/importer.js
+++ controllers/importer.js
@@ -5,45 +5,36 @@ const srt2vtt = require('srt-to-vtt');
 const uuid = require('uuid');
 const path = require('path');
 
-const prom = require("bluebird");
-prom.promisifyAll(fs);
-
 function doImport (req, res) {
 	var location = req.body.location;
-	var promises = [];
-	var list = [];
 
-	fs.readdirAsync(location)
-	.then((files) => {
-		for (var i in files)
-			promises.push(readFiles(location, files[i], list));
-	})
-	.catch(err => {
-		console.error(err);	
-	});
+	var structure = [];
+	getAllFiles(location, structure);
 
-	prom.all(promises).then((something) => {
-		console.log(something);
-	});
+	//console.log(JSON.stringify(tree, null, '    '));
 
 	res.redirect('/');
 }
 
-function readFiles(location, folder, list) {
-	return new prom((resolve, reject) => {
-		if(!location || !folder || !list) {
-			reject(new Error('Missing arguments'));
+function getAllFiles(location, structure) {
+	if (fs.lstatSync(location).isDirectory()){
+		var result = fs.readdirSync(location);
+
+		for (var i in result) {
+			var fullpath = path.join(location, result[i]);
+			var ext = path.extname(fullpath);			
+			if(ext === '.mp4' || ext === '.vtt'){
+				
+			}else {
+				getAllFiles(fullpath);				
+			}
 		}
-		else {
-			var movieFolder = path.join(location, folder);
-			resolve({
-				path: uuid.v4(),
-				name: folder,
-				video: path.join(location, folder),
-				subtitle: path.join(location, folder)
-			});
-		}		
-	});
+	}
+}
+
+function moveMediaFiles(structure){
+	var rt = tree.name;
+
 }
 
 module.exports = {
